17bcc99a18cb5995d2c169c29f3809727f6b66f1
Sathish <sathishvp2495@gmail.com>
2022-03-03 22:28:07 +0530

    b3 value cahnged

--- effect.js
+++ effect.js
@@ -9,7 +9,7 @@ $('document').ready(function(){
 			$('#b1,#b2,#b3,#b4,#b5,#b6,#b7').stop();
 			$('#b11').animate({top:240, left: vw-350},500);
 			$('#b22').animate({top:240, left: vw-200},500);
-			$('#b33').animate({top:240, left: vw-120},500);
+			$('#b33').animate({top:240, left: vw-105},500);
 			$('#b44').animate({top:240, left: vw-10},500);
 			$('#b55').animate({top:240, left: vw+90},500);
 			$('#b66').animate({top:240, left: vw+150},500);
@@ -152,7 +152,7 @@ $('document').ready(function(){
 		$('#b7').attr('id','b77')
 		$('#b11').animate({top:240, left: vw-350},500);
 		$('#b22').animate({top:240, left: vw-200},500);
-		$('#b33').animate({top:240, left: vw-120},500);
+		$('#b33').animate({top:240, left: vw-105},500);
 		$('#b44').animate({top:240, left: vw-10},500);
 		$('#b55').animate({top:240, left: vw+90},500);
 		$('#b66').animate({top:240, left: vw+150},500);
